5f3749e7097ce75b1e381250e6e46dcd5e61a60a
quentin-fouilleul <fouilleul.quentin@outlook.fr>
2025-04-08 14:25:16 +0200

    Initial commit

---
+++ src/main/java/fr/quentin/poppy/Poppy.java
@@ -0,0 +1,16 @@
+package fr.quentin.poppy;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Poppy implements ModInitializer {
+	public static final String MOD_ID = "poppy";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/fr/quentin/poppy/PoppyClient.java
@@ -0,0 +1,10 @@
+package fr.quentin.poppy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PoppyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
